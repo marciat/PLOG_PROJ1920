814a13f4dfbf359f833306e6db6cfe6cce702901
Joao Leite <jopamoleite@gmail.com>
2019-10-15 16:21:17 +0100

    Displayed board

--- nudge.pl
+++ nudge.pl
@@ -1,12 +1,57 @@
 :-use_module(library(lists)).
 
+simbolo(0, ' ').
+simbolo(1, 'B').
+simbolo(2, 'P').
+
+player_symbol(1, 'B').
+player_symbol(2, 'P').
+
 % celulas vazias - 0
 % discos brancos - 1
 % discos pretos - 2
 
 initBoard(X):-
-	L1 = [2,0,2,0,2],
-	L2 = [0,0,0,0,0],
-	L3 = [0,0,0,0,0],
-	L4 = [0,0,0,0,0],
-	L5 = [1,0,1,0,1].
+	X=[[2,0,2,0,2],
+	   [0,0,0,0,0],
+	   [0,0,0,0,0],
+	   [0,0,0,0,0],
+	   [1,0,1,0,1]].
+
+printBoard([]):-
+	write('---------------------').
+	
+printLine([]):-
+	write('|').
+
+printCell([]).
+
+printBoard([L|T]):-
+	write('---------------------'),
+	nl,
+	printLine(L),
+	nl,
+	printBoard(T).
+
+printLine([C|T]):-
+	write('| '),
+	printCell(C),
+	printLine(T).
+
+printCell(C):-
+	simbolo(C,S),
+	write(S),
+	write(' ').
+
+initGame(Board, Player):-
+	initBoard(Board),
+	displayGame(Board,Player),
+	nl.
+
+displayGame(Board, Player):-
+	write('Player '),
+	player_symbol(Player, S),
+	write(S),
+	nl,
+	nl,
+	printBoard(Board).
