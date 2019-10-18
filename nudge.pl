4c9e860a8fa77547fe7a94d7d7d81eee0fb63825
Márcia Teixeira <marcia309@hotmail.com>
2019-10-18 17:52:43 +0100

    Improved board design and code organization; added board coordinates

--- nudge.pl
+++ nudge.pl
@@ -1,18 +1,5 @@
 :-use_module(library(lists)).
-
-simbolo(0, ' ').
-simbolo(1, 'B').
-simbolo(2, 'P').
-
-player_symbol(1, 'B').
-player_symbol(2, 'P').
-
-% celulas vazias - 0
-% discos brancos - 1
-% discos pretos - 2
-
-increment(X, X1) :-
-	X1 is X+1.
+:-include('view.pl').
 
 initBoard(X):-
 	X=[[2,0,2,0,2],
@@ -21,46 +8,6 @@ initBoard(X):-
 	   [0,0,0,0,0],
 	   [1,0,1,0,1]].
 
-printBoard([], LineNr):-
-	write('   ---------------------').
-
-printBoard([L|T], LineNr):-
-	write('   ---------------------'),
-	nl,
-	put_code(LineNr),
-	write('  '),
-	printLine(L),
-	nl,
-	increment(LineNr, NewLineNr),
-	printBoard(T, NewLineNr).
-
-printLine([]):-
-	write('|').
-
-printLine([C|T]):-
-	write('| '),
-	printCell(C),
-	printLine(T).
-
-printCell([]).
-
-printCell(C):-
-	simbolo(C,S),
-	write(S),
-	write(' ').
-
 initGame(Board, Player):-
 	initBoard(Board),
-	displayGame(Board,Player),
-	nl.
-
-displayGame(Board, Player):-
-	nl,
-	write('Player '),
-	player_symbol(Player, S),
-	write(S),
-	nl,
-	nl,
-	write('     A   B   C   D   E'),
-	nl,
-	printBoard(Board, 49).
+	displayGame(Board,Player).
---
+++ view.pl
@@ -0,0 +1,100 @@
+simbolo(0, ' ').
+simbolo(1, 'W').
+simbolo(2, 'B').
+
+player_symbol(1, 'W').
+player_symbol(2, 'B').
+
+% empty cells - 0
+% white discs - 1
+% black discs - 2
+
+increment(X, X1) :-
+	X1 is X+1.
+
+displayGame(Board, Player):-
+	nl,
+	write('     A   B   C   D   E'),
+	nl,
+	printBoard(Board, 49),
+	nl, nl,
+	write('Player '),
+	player_symbol(Player, S),
+	write(S),
+	nl, nl.
+
+printTopBorder(_):-
+	write('   '),
+	put_code(201),
+	put_code(205), put_code(205), put_code(205),
+	put_code(203),
+	put_code(205), put_code(205), put_code(205),
+	put_code(203),
+	put_code(205), put_code(205), put_code(205),
+	put_code(203),
+	put_code(205), put_code(205), put_code(205),
+	put_code(203),
+	put_code(205), put_code(205), put_code(205),
+	put_code(187), nl.
+
+printBoard([L|T], 49):-
+	printTopBorder(_),
+	put_code(49),
+	write('  '),
+	printLine(L),
+	nl,
+	printBoard(T, 50).
+
+
+printBoard([], _):-
+	write('   '),
+	put_code(200),
+	put_code(205), put_code(205), put_code(205),
+	put_code(202),
+	put_code(205), put_code(205), put_code(205),
+	put_code(202),
+	put_code(205), put_code(205), put_code(205),
+	put_code(202),
+	put_code(205), put_code(205), put_code(205),
+	put_code(202),
+	put_code(205), put_code(205), put_code(205),
+	put_code(188), nl.
+
+printBoard([L|T], LineNr):-
+	printDivider(_),
+	put_code(LineNr),
+	write('  '),
+	printLine(L),
+	nl,
+	increment(LineNr, NewLineNr),
+	printBoard(T, NewLineNr).
+
+printLine([]):-
+	put_code(186).
+
+printLine([C|T]):-
+	put_code(186),
+	printCell(C),
+	printLine(T).
+
+printCell([]).
+
+printCell(C):-
+	simbolo(C,S),
+	write(' '),
+	write(S),
+	write(' ').
+
+printDivider(_):-
+	write('   '),
+	put_code(204),
+	put_code(205), put_code(205), put_code(205),
+	put_code(206),
+	put_code(205), put_code(205), put_code(205),
+	put_code(206),
+	put_code(205), put_code(205), put_code(205),
+	put_code(206),
+	put_code(205), put_code(205), put_code(205),
+	put_code(206),
+	put_code(205), put_code(205), put_code(205),
+	put_code(185), nl.
